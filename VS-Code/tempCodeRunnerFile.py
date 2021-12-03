fd959e57c03befbc4aa207feb2f412fec8d05790
mayank0508 <thakurmayank0508@gmail.com>
2021-12-03 20:52:07 +0530

    fibo

--- VS-Code/tempCodeRunnerFile.py
+++ VS-Code/tempCodeRunnerFile.py
@@ -1,5 +1,6 @@
-    #    nth = n1 + n2
-    #    # update values
-    #    n1 = n2
-    #    n2 = nth
-    #    count += 1
+    # #    nth = n1 + n2
+    # #    # update values
+    # #    n1 = n2
+    # #    n2 = nth
+    # #    count += 1
+    # affbg
