f8b0b7887dd6abd77720911665d6f239b22d0bab
mayank0508 <thakurmayank0508@gmail.com>
2021-12-04 19:40:09 +0530

    character-aplahabet

---
+++ VS-Code/patter-alpha.py
@@ -0,0 +1,10 @@
+#print kth alphabet
+k = int(input())
+x = ord('A')
+getChar = x + k - 1
+printChar = chr(getChar)
+print(printChar)
+
+# this above method is used to generate Alphabet from numbers
+
+############################################################################################################
