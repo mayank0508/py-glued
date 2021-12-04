a548fcc577da220f257e3cb295008535baeb5809
mayank0508 <thakurmayank0508@gmail.com>
2021-12-04 12:28:24 +0530

    triangular-pattern

--- VS-Code/triangluar-pattern.py
+++ VS-Code/triangluar-pattern.py
@@ -1,21 +1,31 @@
+# # n = int(input())
+# # i = 1
+# # while(i<=n):
+# #     j = 1
+# #     while(j<=i):
+# #         print(j, end='')
+# #         j = j + 1
+# #     print()
+# #     i = i+1
+
 # n = int(input())
 # i = 1
+# p = 1
 # while(i<=n):
 #     j = 1
 #     while(j<=i):
-#         print(j, end='')
-#         j = j + 1
+#         print(p, end='')
+#         j = j+1
+#         p = p +1
 #     print()
-#     i = i+1
+#     i = i + 1
 
 n = int(input())
 i = 1
-p = 1
 while(i<=n):
     j = 1
     while(j<=i):
-        print(p, end='')
-        j = j+1
-        p = p +1
+        print(i-j+1, end='')
+        j = j + 1
     print()
-    i = i + 1
+    i = i+1
