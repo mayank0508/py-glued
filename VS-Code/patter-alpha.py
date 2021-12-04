504b0b4fab44c9e4feaf712c007d09d7e819b5d5
mayank0508 <thakurmayank0508@gmail.com>
2021-12-04 22:57:05 +0530

    character-aplahabet

--- VS-Code/patter-alpha.py
+++ VS-Code/patter-alpha.py
@@ -27,19 +27,68 @@
 
 ################################################################################################################    
 
-n = int(input())
-i = 1
-while(i<=n):
-    j = 1
-    start_char = chr(ord('A') + i - 1)
-    while(j<=n):
-        CharP = chr(ord(start_char) + j - 1)
-        print(CharP, end='')
-        j = j + 1
-    print()
-    i = i + 1
+# n = int(input())
+# i = 1
+# while(i<=n):
+#     j = 1
+#     start_char = chr(ord('A') + i - 1)
+#     while(j<=n):
+#         CharP = chr(ord(start_char) + j - 1)
+#         print(CharP, end='')
+#         j = j + 1
+#     print()
+#     i = i + 1
     
     # ABCD
     # BCDE
     # CDEF
     # DEFG
+
+##############################################################################################
+
+# n = int(input())
+# i = 1
+# while(i<=n):
+#     j = 1
+#     start_char = chr(ord('A') + i - 1)
+#     while(j<=i):
+#         CharP = chr(ord(start_char) + j - 1)
+#         print(CharP, end='')
+#         j = j + 1
+#     print()
+#     i = i + 1
+    
+    # A
+    # BC
+    # CDE
+    # DEFG
+
+##############################################################################################
+
+# n = int(input())
+# i = 1
+# while(i<=n):
+#     j = 1
+#     start_char = chr(ord('A') + i - 1)
+#     while(j<=i):
+#         CharP = chr(ord(start_char))
+#         print(CharP, end='')
+#         j = j + 1
+#     print()
+#     i = i + 1
+
+# A
+# BB
+# CCC
+
+#############################################################################################
+
+# n = int(input())
+# i = 1
+# while(i<=n):
+#     j = 1
+#     while(j<=n):
+#         print(1, end='')
+#         j = j + 1
+#     print()
+#     i = i + 1
