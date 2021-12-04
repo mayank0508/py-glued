c8bcaacf03c26eea80ddc08b56618177e3fd2cf2
mayank0508 <thakurmayank0508@gmail.com>
2021-12-04 19:55:36 +0530

    character-aplahabet

--- VS-Code/patter-alpha.py
+++ VS-Code/patter-alpha.py
@@ -1,21 +1,45 @@
-#print kth alphabet
-k = int(input())
-x = ord('A')
-getChar = x + k - 1
-printChar = chr(getChar)
-print(printChar)
+# #print kth alphabet
+# k = int(input())
+# x = ord('A')
+# getChar = x + k - 1
+# printChar = chr(getChar)
+# print(printChar)
 
 # this above method is used to generate Alphabet from numbers
 
 ############################################################################################################
 
+# n = int(input())
+# i = 1
+# while(i<=n):
+#     j = 1
+#     while(j<=n):
+#         CharP = chr(ord('A') + j - 1)
+#         print(CharP, end='')
+#         j = j + 1
+#     print()
+#     i = i + 1
+
+    # ABCD
+    # ABCD
+    # ABCD
+    # ABCD   ---> this is the o/p of this code
+
+################################################################################################################    
+
 n = int(input())
 i = 1
 while(i<=n):
     j = 1
+    start_char = chr(ord('A') + i - 1)
     while(j<=n):
-        CharP = chr(ord('A') + j - 1)
+        CharP = chr(ord(start_char) + j - 1)
         print(CharP, end='')
         j = j + 1
     print()
     i = i + 1
+    
+    # ABCD
+    # BCDE
+    # CDEF
+    # DEFG
