b9209d91a7902a8f0618fec48185cae919759984
mayank0508 <thakurmayank0508@gmail.com>
2021-12-04 11:02:50 +0530

    pattern

--- VS-Code/nthpatter.py
+++ VS-Code/nthpatter.py
@@ -1,3 +1,13 @@
+# n = int(input('Enter Numbers'))
+# i = 1
+# while(i<=n):
+#         j = 1
+#         while(j<=n):
+#             print(n,end='')
+#             j = j+1
+#         print()
+#         i = i+1
+
 n = int(input('Enter Numbers'))
 i = 1
 while(i<=n):
