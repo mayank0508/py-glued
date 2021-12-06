f8826b6b383a9b1b9263431aeb2982ea7868dfb5
mayank0508 <thakurmayank0508@gmail.com>
2021-12-06 17:52:11 +0530

    star-pat

--- VS-Code/inverted-patter.py
+++ VS-Code/inverted-patter.py
@@ -32,3 +32,22 @@
 
 ##############################################################
 
+n = int(input())
+i = 1
+while(i<=n):
+    space = 1
+    while(space<= n-i):
+        print(' ', end ="")
+        space = space + 1
+    star = 1
+    while(star <= i):
+        print('*', end='')
+        star = star + 1
+    print()
+    i = i + 1
+
+    #     *
+    #    **
+    #   ***
+    #  ****
+    # *****
