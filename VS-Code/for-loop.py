374212c41d4cc6564742974420ecc49a7b183a6d
mayank0508 <thakurmayank0508@gmail.com>
2021-12-09 17:08:39 +0530

    for-loop

--- VS-Code/for-loop.py
+++ VS-Code/for-loop.py
@@ -1,5 +1,28 @@
-# a < b (given)
-n = int(input())
-m = int(input())
-for i in range(n, m+1, 1):
-    print(i)
+# # a < b (given)
+# n = int(input())
+# m = int(input())
+# for i in range(n, m+1, 1):
+#     print(i)
+
+################################################################
+
+# n = int(input())
+
+# for i in range(1, n+1, 1):
+#     for s in range(n-i):
+#         print(" ", end='')
+#     for j in range(i, 2*i, 1):
+#         print(j, end='')
+#     for k in range(2*i-2, i-1, -1): # here i -1 is the tricky part of the this question 
+#         print(k, end='')
+#     print()
+
+#     1
+#    232
+#   34543
+#  4567654
+# 567898765
+
+################################################################
+
+# 
