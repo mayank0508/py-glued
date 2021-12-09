da9462e436208349002770ecdaa7e8c39fec60da
mayank0508 <thakurmayank0508@gmail.com>
2021-12-09 20:09:54 +0530

    break

---
+++ VS-Code/break.py
@@ -0,0 +1,13 @@
+n = int(input())
+d = 2
+Flag = False;
+while(n > d):
+        if(n%d == 0):
+            Flag = True
+            break
+        d = d+1
+
+if Flag:
+    print("No Prime Bitach 😂😂🤷‍♂️")
+else:
+    print('Take this L mf😲🤡 its a Prime 🤐')
