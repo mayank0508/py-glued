23ceab6980cd067860ea0725e891e38d9ed48521
mayank0508 <thakurmayank0508@gmail.com>
2021-12-10 17:22:59 +0530

    while-else

--- VS-Code/prime-or-not-and-then-print-all.py
+++ VS-Code/prime-or-not-and-then-print-all.py
@@ -1,13 +1,24 @@
-n = int(input())
-k = 2
-while(n>=k):
-    d = 2
-    Flag = False;
-    while(k > d):
-        if(k%d == 0):
-            Flag = True
-        d = d+1
-    if not(Flag):
-        print(k)
-    k = k+1
+# n = int(input())
+# k = 2
+# while(n>=k):
+#     d = 2
+#     Flag = False;
+#     while(k > d):
+#         if(k%d == 0):
+#             Flag = True
+#         d = d+1
+#     if not(Flag):
+#         print(k)
+#     k = k+1
         
+############################################################
+
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    
+    return leap
+
+year = int(input())
+print(is_leap(year))
