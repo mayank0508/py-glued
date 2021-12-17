56ab14d81a7968956dfab4229d61f6a02f62d0b7
mayank0508 <thakurmayank0508@gmail.com>
2021-12-17 20:06:28 +0530

    function ass

--- VS-Code/functions.py
+++ VS-Code/functions.py
@@ -1,23 +1,50 @@
-# Is the number Prime or Not
+# # Is the number Prime or Not
 
-def isPrime(n):
-    for i in range(2, n):
-        if (n%i==0):
-            break
-    else:
-        return True
-    return False
+# def isPrime(n):
+#     for i in range(2, n):
+#         if (n%i==0):
+#             break
+#     else:
+#         return True
+#     return False
 
-# print(isPrime(4))
+# # print(isPrime(4))
 
-########################################################################
+# ########################################################################
 
-# Print all the prime numbers till the end 
+# # Print all the prime numbers till the end 
 
-def Primetill(n):
-    for i in range(2, n+1):
-        is_Prime = isPrime(i)
-        if is_Prime:
-            print(i)
+# def Primetill(n):
+#     for i in range(2, n+1):
+#         is_Prime = isPrime(i)
+#         if is_Prime:
+#             print(i)
 
-print(Primetill(20))
+# print(Primetill(20))
+
+################################################################################################
+
+
+
+
+def printTable(start,end,step):
+    s = start
+    e = end
+    st = step
+    for i in range(s,e+1,st):
+        x = (5/9)*(i - 32)
+        z = int(x)
+        print(i, z)
+
+    pass 
+
+	   
+s = int(input())
+e = int(input())
+step = int(input())
+printTable(s,e,step)
+
+
+
+
+
